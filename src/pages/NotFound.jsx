c99ed51efe1b8e7e754cea21a2ecd94f4d655507
Pedro RivDom <66123144+RivDomCode@users.noreply.github.com>
2022-04-17 21:04:25 +0200

    nothing found done

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,17 @@
-import React from "react";
+import React from 'react'
+import { EmojiSadIcon } from "@heroicons/react/outline";
+
 
 export const NotFound = () => {
-  return <div>NotFound</div>;
-};
+  return (
+    <div className="bg-slate-800 flex items-center h-screen justify-center">
+                <p><EmojiSadIcon className='text-orange-500 h-24'/></p>
+        <div className="font-black text-7xl  bg-clip-text text-transparent bg-gradient-to-r from-orange-500 to-orange-100 p-2 tracking-wider uppercase">
+            404 Nothing Found!!!
+        </div>
+    <br />
+
+    </div>
+
+  )
+}
